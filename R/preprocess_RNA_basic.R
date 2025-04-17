2947dcc4f49815ac71edcc419ee4b54aef70002e
Mike Martinez <mike.j.martinez99@gmail.com>
2025-04-17 16:30:26 -0400

    Added preprocess_RNA_basic unit tests

--- R/preprocess_RNA_basic.R
+++ R/preprocess_RNA_basic.R
@@ -16,19 +16,41 @@
 #'
 #' @examples # Run on a single seurat object
 #' @examples subset <- preprocess_RNA_basic(x, "Sample_1", nCount_thresh = 250, mito_gene_prefix = "^mt-")
-preprocess_RNA_basic <- function(x, ident, nCount_thresh=300, mito_gene_prefix) {
+preprocess_RNA_basic <- function(x, ident, nCount_thresh = 300, mito_gene_prefix) {
   print(ident)
   
-  # Subset based on nCount RNA
-  xSub <- subset(x,
-                 subset = nCount_RNA >= nCount_thresh)
+  #----- Check for presence of nCount_RNA
+  if ("nCount_RNA" %in% colnames(x@meta.data)) {
+    
+    # Count how many cells are below the threshold
+    numCellsFail <- sum(x@meta.data$nCount_RNA < nCount_thresh)
+    
+    if (numCellsFail == 0) {
+      stop("No cells found")
+    } else {
+      message(paste0(numCellsFail, " cells were below nCount_thresh >= ", nCount_thresh))
+    }
+    
+    # Subset based on nCount_RNA
+    xSub <- subset(x, subset = nCount_RNA >= nCount_thresh)
+    
+  } else {
+    stop("None of the requested variables were found")
+  }
   
+  #----- Check for mitochondrial genes
+  mito_genes <- grep(mito_gene_prefix, rownames(xSub), value = TRUE)
+  if (length(mito_genes) == 0) {
+    warning(paste0("No mitochondrial genes found using pattern '", mito_gene_prefix,
+                   "' in sample ", ident))
+  } else {
+    message(paste0(length(mito_genes), " mitochondrial genes found in sample ", ident))
+  }
   
-  # Add mito qc column ("^mt-")
-  xSub$percentMT <- PercentageFeatureSet(object = xSub,
-                                         pattern = mito_gene_prefix)
+  #----- Add percent mitochondria column
+  xSub$percentMT <- PercentageFeatureSet(object = xSub, pattern = mito_gene_prefix)
   
-  # Add log10 gene/UMI (complexity measure of dataset)
+  #----- Add log10 genes per UMI
   xSub$log10GenesPerUMI <- log10(xSub$nFeature_RNA) / log10(xSub$nCount_RNA)
   
   return(xSub)
