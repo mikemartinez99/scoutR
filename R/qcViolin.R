fbe00bc7c7f981389c3dd0da5217453931d748ce
Mike Martinez <mike.j.martinez99@gmail.com>
2025-06-18 21:41:07 -0400

    Fixed qcViolin typos

--- R/qcViolin.R
+++ R/qcViolin.R
@@ -16,19 +16,21 @@
 #' @param logTransform Logical, whether or not to log transform the y-axis
 #' @param sampleColors A color vector for samples
 #' @param figDir A directory path ending in "/" to hold output files
+#' @param width Figure width
+#' @param height Figure height
 #'
 #' @returns A ggplot object
 #' 
 #' @examples # Generate individual plots as well as a combined one
-#' @examples arrangedViolins <- ggarrange(qcVioli(metadata, "pct_reads_in_peaks", logTransform = FALSE, colors, figDir),
-#' @examples qcViolin(metadata, "atac_peak_region_fragments", logTransform = TRUE, colors, figDir),
-#' @examples qcViolin(metadata, "TSS.enrichment", logTransform = FALSE, colors, figDir),
-#' @examples qcViolin(metadata, "nucleosome_signal", logTransform = FALSE, colors, figDir),
-#' @examples qcViolin(metadata, "nFeature_peaks", logTransform = FALSE, colors, figDir))
+#' @examples arrangedViolins <- ggarrange(qcVioli(metadata, "pct_reads_in_peaks", logTransform = FALSE, colors, figDir, width = 8, height = 8),
+#' @examples qcViolin(metadata, "atac_peak_region_fragments", logTransform = TRUE, colors, figDir, width = 8, height = 8),
+#' @examples qcViolin(metadata, "TSS.enrichment", logTransform = FALSE, colors, figDir, width = 8, height = 8),
+#' @examples qcViolin(metadata, "nucleosome_signal", logTransform = FALSE, colors, figDir, width = 8, height = 8),
+#' @examples qcViolin(metadata, "nFeature_peaks", logTransform = FALSE, colors, figDir, width = 8, height = 8))
 #' @examples ggsave(paste0(figDir, "ATAC_Violin_Summary.png"), arrangedViolins, width = 16, height = 10)
 
 
-qcViolin <- function(metadata, variable, logTransform, sampleColors, figDir) {
+qcViolin <- function(metadata, variable, logTransform, sampleColors, figDir, width = width, height = height) {
   
   # Convert `variable` to a symbol
   variable <- ensym(variable)
@@ -36,7 +38,7 @@ qcViolin <- function(metadata, variable, logTransform, sampleColors, figDir) {
   
   
   if (logTransform == TRUE) {
-    p1 <- ggplot(combinedMeta, aes(x = orig.ident, y = log(.data[[as_string(variable)]]), fill = orig.ident)) +
+    p1 <- ggplot(metadata, aes(x = orig.ident, y = log(.data[[as_string(variable)]]), fill = orig.ident)) +
       geom_point(position = position_jitter(height = 0.2, width = 0.2), shape = 21, size = 1, alpha = 0.6) +
       geom_violin(alpha = 0.7) +
       geom_boxplot(outliers = FALSE, width = 0.2) +
@@ -45,16 +47,16 @@ qcViolin <- function(metadata, variable, logTransform, sampleColors, figDir) {
            x = "", 
            fill = "Sample",
            title = label) +
-      scale_fill_manual(values = colors) +  
+      scale_fill_manual(values = sampleColors) +  
       theme(axis.text = element_text(angle = 90, face = "bold"),
             legend.position = "none")
     
     # Set filename
     fileName <- paste0("Log_", variable, "_QC_Violins.png")
-    ggsave(paste0(violinDir, fileName), p1, width = 8, height = 8)
+    ggsave(paste0(figDir, fileName), p1, width = 8, height = 8)
     
   } else {
-    p1 <- ggplot(combinedMeta, aes(x = orig.ident, y = .data[[as_string(variable)]], fill = orig.ident)) +
+    p1 <- ggplot(metadata, aes(x = orig.ident, y = .data[[as_string(variable)]], fill = orig.ident)) +
       geom_point(position = position_jitter(height = 0.2, width = 0.2), shape = 21, size = 1, alpha = 0.6) +
       geom_violin(alpha = 0.7) +
       geom_boxplot(outliers = FALSE, width = 0.2) +
@@ -63,13 +65,13 @@ qcViolin <- function(metadata, variable, logTransform, sampleColors, figDir) {
            x = "", 
            fill = "Sample",
            title = label) +
-      scale_fill_manual(values = colors) +  
+      scale_fill_manual(values = sampleColors) +  
       theme(axis.text = element_text(angle = 90, face = "bold"),
             legend.position = "none")
     
     # Set filename
     fileName <- paste0(variable, "_QC_Violins.png")
-    ggsave(paste0(figDir, fileName), p1, width = 8, height = 8)
+    ggsave(paste0(figDir, fileName), p1, width = width, height = height)
     
   }
   return(p1)
